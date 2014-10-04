3563b99d73dfff26645701e5d8c9583b71a7d113
Victor Mendonça <androidmend@gmail.com>
2014-10-03 23:13:59 -0400

    bat file no longer used

--- phpdoc.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-REM home dir of php installation
-cd C:\Program Files (x86)\iis express\PHP\v5.4
-phpdoc.bat -d "C:\inetpub\wwwroot\teste" -t "C:\inetpub\wwwroot\teste\phpdoc" -i "C:\inetpub\wwwroot\teste\nbproject\*","C:\inetpub\wwwroot\teste\logs\*","C:\inetpub\wwwroot\teste\vendor\*" --title "DBManagerLogger Docs"
